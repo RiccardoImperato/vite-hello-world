2b1268247a61c596c093e88bae2a9d3218c20814
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-06 17:06:29 +0100

    feat: componente AppTitle e  messaggio in pagina

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  data() {
+    return {
+      message: 'La mia prima App con Vite'
+    };
+  },
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+  <h2>{{ message }}</h2>
+</template>
+
+<style scoped>
+h2 {
+  color: gray;
+}
+</style>
