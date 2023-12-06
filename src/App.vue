234b1cae42c220f80ba41385dbba78d7eed5fee1
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-06 23:01:34 +0100

    feat: componente AppHero (jumbotron)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import AppTitle from './components/AppTitle.vue';
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 
 export default {
   data() {
@@ -8,13 +9,15 @@ export default {
     };
   },
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
   <h2>{{ message }}</h2>
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {};
+</script>
+
+<template>
+    <div>
+        <img src="../assets/vue.svg">
+        <img src="../assets/vite.svg">
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 200px;
+    margin: 0 20px;
+}
+
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 300px;
+    width: 500px;
+    background-color: #1e2a4b;
+    border-radius: 15px;
+}
+</style>
